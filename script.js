16e8af9d5f732c7f9cacc19e3796a9e4dd489da4
Harikesh Patel <43793294+kumarharikesh@users.noreply.github.com>
2022-10-08 21:13:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-// const textInput = document.querySelector("#txt-input");
-// const output = document.querySelector("#output");
-// const form = document.querySelector("#form");
-
-// let URL = "https://api.funtranslations.com/translate/vulcan.json";
-
-// const getURL = (text) => `${URL}?text=${text}`;
-
-// function errorHandler(error){
-//   alert("Some error occured in the server.\nPlease try again after some time !\nSorry for the inconvenience. 🙂")
-// }
-
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-
-//   const inputText = textInput.value;
-
-//   fetch(getURL(inputText))
-//     .then((response) => response.json())
-//     .then((json) => (output.innerText = json.contents.translated))
-//     .catch(errorHandler);
-// });
-
 let url = "https://api.funtranslations.com/translate/vulcan.json"
 let translate=document.querySelector("#translate");
 let erase=document.querySelector("#clear");
